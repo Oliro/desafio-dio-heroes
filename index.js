0bf940e52d5f17ca38a4b23696e179988a679ae4
Fernando OR <fernando.or6@gmail.com>
2024-06-04 17:35:06 -0300

    Concluido

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('teste')
+const heroi = { nome: "SuperBit", nivel: 8200 };
+
+const niveis = [
+    { nivelMin: 0, nivelMax:1000, descricao: "Ferro" },
+    { nivelMin: 1001, nivelMax:2000, descricao: "Bronze" },
+    { nivelMin: 2001, nivelMax:5000, descricao: "Prata" },
+    { nivelMin: 5001, nivelMax:7000, descricao: "Ouro" },
+    { nivelMin: 7001, nivelMax:8000, descricao: "Platina" },
+    { nivelMin: 8001, nivelMax:9000, descricao: "Ascendente" },
+    { nivelMin: 9001, nivelMax:10000, descricao: "Imortal" },
+    { nivelMin: 10001, nivelMax:10001, descricao: "Radiante" },
+];
+
+niveis.forEach(element => {
+    if (heroi.nivel > element.nivelMin && heroi.nivel < element.nivelMax) {
+        console.log("O Herói de nome " +  heroi.nome + ", está no nível de " + element.descricao  +".");
+    }
+});
+
